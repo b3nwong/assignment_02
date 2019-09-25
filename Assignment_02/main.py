08d9caa1f4b133e6a64835b6685c9ce2b090c934
b3nwong <53283480+b3nwong@users.noreply.github.com>
2019-09-25 13:12:41 +0800

    add format layout of kiv app

--- Assignment_02/main.py
+++ Assignment_02/main.py
@@ -10,6 +10,16 @@ class TravelTrackerApp(App):
         self.root = Builder.load_file('app.kv')
         return self.root
 
-    def sort_method_
+  #  def sort_method_priority(self):
+    # call sorting method from placecollection
+    #change display on sorting label
+
+    def clear_input(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.input_country.text = ""
+        self.root.ids.input_priority.text = ""
+
+
+
 
 TravelTrackerApp().run()
